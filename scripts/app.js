712679ce78b84e7a77c36099d14fedf7bec70992
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-08-01 21:22:43 +0300

    Setup basic structure with templates

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        const href = window.location.href;
+        const uri = href.substr(href.lastIndexOf('/') + 1);
+
+            const current = document.querySelector("a[href='" + uri + "']");
+            if (current !== null) {
+                current.className += ' active';
+            }
+    }
+};
