46da619fb2829b0cf17441a181aad934db064b18
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-08-02 07:44:26 +0300

    Setup basic, login and logout flow

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
-const getToken = () => {
-    return localStorage.getItem('auth-token');
-};
+const API_URL = 'https://mdiary-v1.herokuapp.com/api/v1/';
 
-const setToken = (value) => {
-    return localStorage.setItem('auth-token', value)
-};
+const apiUrl = (path) => API_URL + path;
+
+const getToken = () => localStorage.getItem('auth-token');
+
+const setToken = (value) => localStorage.setItem('auth-token', value);
 
 const checkAuth = (uri) => {
     // the landing, sign up and sign in pages are the only unprotected pages
-    if (uri === 'index.html' || uri === 'signUp.html' || uri === 'signin.html') {
+    if (uri === 'index.html' || uri === 'signup.html' || uri === 'signin.html') {
         return
     }
     if (!getToken()) {
@@ -17,12 +17,108 @@ const checkAuth = (uri) => {
     }
 };
 
-const signUp = () => {
+const getElement = (id) => document.getElementById(id);
 
+const getValue = (id) => getElement(id).value;
+
+const logout = (event) => {
+    event.preventDefault();
+    if (getToken()) {
+        localStorage.removeItem('auth-token');
+    }
+    window.location = 'index.html';
 };
 
-const login = () => {
+const postSignUp = (body) => {
+    let responseOk = false;
+    let responseStatus = 0;
+    fetch(apiUrl('signup'), {
+        method: "post",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            responseOk = response.ok;
+            responseStatus = response.status;
+            return response.json();
+        })
+        .then(data => {
+            if (responseOk) {
+                console.log('DISPLAY SUCCESSFULLY SIGNED IN')
+            } else {
+                switch (responseStatus) {
+                    case 422:
+                        console.log('DISPLAY ERRORS');
+                        break;
+                    case 409:
+                        console.log('USER EXISTS');
+                        break;
+                    case 500:
+                        console.log('OOPS :( SOMETHING WENT WRONG');
+                }
+                console.log(data)
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+};
 
+const signUp = (ev) => {
+    ev.preventDefault();
+    let name = getValue('name');
+    let email = getValue('email');
+    let password = getValue('password');
+    // let password_conf = getValue('password_conf');
+    postSignUp({name: name, email: email, password: password});
+};
+
+const postLogin = (body) => {
+    let responseOk = false;
+    let responseStatus = 0;
+    fetch(apiUrl('login'), {
+        method: "post",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            responseOk = response.ok;
+            responseStatus = response.status;
+            return response.json();
+        })
+        .then(data => {
+            if (responseOk) {
+                // Login successful
+                setToken(data.token);
+                window.location = 'entries.html';
+            } else {
+                switch (responseStatus) {
+                    case 422:
+                        console.log('DISPLAY ERRORS');
+                        break;
+                    case 401:
+                        console.log('INVALID LOGIN');
+                        break;
+                    case 500:
+                        console.log('OOPS :( SOMETHING WENT WRONG');
+                }
+                console.log(data)
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+};
+
+const login = (ev) => {
+    ev.preventDefault();
+    let email = getValue('email');
+    let password = getValue('password');
+    postLogin({email: email, password: password})
 };
 
 const fetchEntries = () => {
