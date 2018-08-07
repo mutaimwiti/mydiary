e2e3b5c23f21c6e4307547941ba89094bcef58ff
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-08-07 21:14:32 +0300

    Fix address resolution on GitHub Pages

--- src/router.js
+++ src/router.js
@@ -5,8 +5,13 @@ export const getUri = () => {
     return url.substr(url.lastIndexOf('/') + 1);
 };
 
-// credit Stack overflow: https://stackoverflow.com/questions/6941533/get-protocol-domain-and-port-from-url
-const getAddress = () => location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
+const getAddress = () => {
+    let address = window.location.href;
+    if (address.includes('entry')) {
+        address = address.substring(0, address.lastIndexOf('entry'))
+    }
+    return address.substring(0, address.lastIndexOf('/'));
+};
 
 export const redirect = (page='index.html') => window.location = `${getAddress()}/${page}`;
 
