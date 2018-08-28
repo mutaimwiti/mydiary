00f8f220fada898bf5ce62bb68994d4b1e201d7b
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-08-28 21:50:43 +0300

    Implement profile page

--- src/modules/Profile.js
+++ src/modules/Profile.js
@@ -1,5 +1,36 @@
+import DOM from "./DOM";
+import API from "./API";
+import Error from "./Error";
+import {timeSince} from "./helpers";
+
 export default class Profile {
     static init() {
+        API.message('Loading profile').get('profile', this);
+    }
 
+    static handle(ok, code, data) {
+        if (ok) {
+            this.display(data);
+        } else {
+            Error.handle(code, data);
+        }
+    }
+
+    static display({name, since, entry_count, latest_entry}) {
+        let count = `You have no entries...<a href="entries/create.html">create entry</a>.`, latest = ``;
+        if (entry_count) {
+            count = `Number of entries: ${entry_count}`;
+            latest = `Latest entry: <a href="entries/view.html?id=${latest_entry.id}">${latest_entry.title}</a>`;
+        }
+        DOM.html('#profile',
+            `<div class="panel">
+                <div class="panel-body">
+                    <b><h4>${name}</h4></b>
+                    <h5>Joined: ${timeSince(`${since} UTC`)}</h5>
+                    <h5>${count}</h5>
+                    <h5>${latest}</h5>
+                </div>
+            </div>`
+        );
     }
 }
